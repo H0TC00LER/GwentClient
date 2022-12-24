9bd8f76763c0f804252ca39d5ab7d398b1236557
H0TC00LER <112895111+H0TC00LER@users.noreply.github.com>
2022-12-24 05:44:12 +0300

    added player status

--- GwentClient/GwentClient/Models/CardLibrary.cs
+++ GwentClient/GwentClient/Models/CardLibrary.cs
@@ -8,8 +8,8 @@ namespace GwentClient.Models
         public static Dictionary<int, Card> Cards { get; } =
             new()
             {
-                {1, new Card(1, Role.Melee, 3, "Geralt")},
-                {2, new Card(2, Role.Shooter, 2, "Archer")}
+                {1, new Card(1, Role.Melee, 3, "Геральт")},
+                {2, new Card(2, Role.Shooter, 2, "Лучник")}
             };
     }
 }
--- GwentClient/GwentClient/ViewModels/MainWindowViewModel.cs
+++ GwentClient/GwentClient/ViewModels/MainWindowViewModel.cs
@@ -1,10 +1,13 @@
-﻿
-using Avalonia.Controls;
+﻿using System.Collections.Generic;
 
 namespace GwentClient.ViewModels
 {
     public class MainWindowViewModel : ViewModelBase
     {
-
+        public GameFieldViewModel GameField { get; }
+        public MainWindowViewModel()
+        {
+            GameField = new GameFieldViewModel(new List<int>() { 1, 2, 1, 1, 2 }, "Пупсич", "Папич");
+        }
     }
 }
